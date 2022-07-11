4079377aab94f6b41e6da4665598c85e8e1f9177
katerine <7241katigmail.com>
2022-07-10 23:51:34 -0400

    katerineondarza-8578021

---
+++ src/App.stories.jsx
@@ -0,0 +1,7 @@
+import App from './App'
+export default {
+    title: 'KaterineOndarza/page',
+    component: App,
+}
+const Template = args => <App  {...args}/>
+export const app= Template.bind({});
---
+++ src/components/home/Home.stories.jsx
@@ -0,0 +1,8 @@
+import Home from './Home'
+export default {
+    title: 'KaterineOndarza/page',
+    component: Home,
+    
+}
+const Template = args => <Home  {...args}/>
+export const home= Template.bind({});
